768227746db599762db5f799216df90b3e81cae9
Rodrigo Ipince <rodrigo.ipince@gmail.com>
2020-05-03 11:48:47 -0700

    Back off if requests are taking too long; parametrize sleep time in between requests

--- ivss/ivss_scrape.py
+++ ivss/ivss_scrape.py
@@ -10,19 +10,19 @@ from HTMLParser import HTMLParser
 
 import ivss_utils
 
-SLEEP_TIME_SEC = 1
+SLEEP_TIME_SEC = 2
 HTML = HTMLParser()
 DUMMY_RESPONSE_CONTENT = 'dummy content'
 
-def scrape_randomly(beg, end, directory, known=None, force=False, dry_run=True):
+def scrape_randomly(beg, end, directory, known=None, wait=None, force=False, dry_run=True):
   skip = set()
   if known:
     skip.update(ivss_utils.read_batch(known))
   print "found %s ids in skip file" % len(skip)
   cedulas = [('V', x) for x in range(beg, end) if ('V', x) not in skip]
-  scrape(cedulas, directory, force=force, dry_run=dry_run)
+  scrape(cedulas, directory, wait=wait, force=force, dry_run=dry_run)
  
-def scrape(cedulas, directory, force=False, dry_run=True):
+def scrape(cedulas, directory, wait=None, force=False, dry_run=True):
   # randomize
   random.shuffle(cedulas)
   directory = directory + '-cache' if not dry_run else directory + '-dummy'
@@ -33,7 +33,7 @@ def scrape(cedulas, directory, force=False, dry_run=True):
       print "Reading from cached file: %s" % filepath
       #contents = file(filepath).read()
     else:
-      contents = fetch(cedula, filepath, dry_run=dry_run)
+      contents = fetch(cedula, filepath, sleep=wait, dry_run=dry_run)
     #print contents
 
 def fetch(cedula, filepath, sleep=SLEEP_TIME_SEC, dry_run=True):
@@ -53,6 +53,10 @@ def fetch(cedula, filepath, sleep=SLEEP_TIME_SEC, dry_run=True):
   outfile = file(filepath, 'w')
   response = HTML.unescape(response)
   outfile.write(response.encode('utf8'))
+
+  # Back off if the request took too long.
+  if elapsed > 6:
+    time.sleep(20)
   time.sleep(sleep)
   return response
 
@@ -65,6 +69,7 @@ if __name__ == '__main__':
   parser.add_option("-d", "--dry_run", dest="dry_run", action="store_true")
   parser.add_option("-i", "--input", dest="input_file", type="str")
   parser.add_option("-k", "--known", dest="known", type="str")
+  parser.add_option("-w", "--wait", dest="wait", type="int")
   (options, args) = parser.parse_args()
 
   if not options.input_file:
@@ -73,6 +78,6 @@ if __name__ == '__main__':
 
   if options.input_file:
     cedulas = ivss_utils.read_batch(options.input_file)
-    scrape(cedulas, options.input_file, force=options.force, dry_run=options.dry_run)
+    scrape(cedulas, options.input_file, wait=options.wait, force=options.force, dry_run=options.dry_run)
   else:
-    scrape_randomly(options.beginning, options.end, 'seq', known=options.known, force=options.force, dry_run=options.dry_run)
+    scrape_randomly(options.beginning, options.end, 'seq', known=options.known, wait=options.wait, force=options.force, dry_run=options.dry_run)
